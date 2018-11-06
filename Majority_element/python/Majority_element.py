cd41afd295566e61db2f205622a163eef1eda38b
Wen-Ting Wang <egpivo@gmail.com>
2018-11-06 11:13:16 +0800

    Update the solution

--- Majority_element/python/Majority_element.py
+++ Majority_element/python/Majority_element.py
@@ -5,13 +5,35 @@ class Solution:
         :rtype: int
         """
         for i in set(nums):
-            if nums.count(i) >= len(nums)/2:
+            if nums.count(i) > len(nums)/2:
                 return i
 
         return -1
+
+# Moore voting
+class Solution2:
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        count, mode = 1, nums[0]
+        for i, val in enumerate(nums):
+          if val == mode:
+            count += 1
+          else:
+            if count == 0:
+              mode = nums[i]
+              count = 1
+            else:
+              count -= 1
+
+        return mode
 
 
 if __name__ == "__main__":
   nums = [2,2,1,1,1,2,2]
   ans = Solution()
   print(ans.majorityElement(nums))
+  ans2 = Solution2()
+  print(ans2.majorityElement(nums))
