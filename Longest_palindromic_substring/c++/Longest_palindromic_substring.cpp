bf85ff1bd6873e376aadf4fb793c770d2c6efaba
Wen-Ting Wang <egpivo@gmail.com>
2018-11-21 11:04:43 +0800

    Add a new leetcode problem

---
+++ Longest_palindromic_substring/c++/Longest_palindromic_substring.cpp
@@ -0,0 +1,53 @@
+//
+//  Longest_palindromic_substring.cpp
+//  
+//
+//  Created by Wen-Ting Wang on 2018/11/21.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    string longestPalindrome(string s){
+        int len = s.size();
+        if(len < 2) return s;
+        
+        int start = 0, end = 0; // initialize the recording parameters
+        
+        for(int i = 0; i < len - 1; ++i){
+            Solution::searchPalindrome(s, len, i, i, start, end); // for odd length
+            Solution::searchPalindrome(s, len, i, i + 1, start, end); //for even length
+        }
+        return s.substr(start, end);
+    }
+    
+    
+    void searchPalindrome(const string s, const int len, int left, int right, int &start, int &end){
+        
+        while(left >= 0 && right < len && s[left] == s[right]){
+            -- left;
+            ++ right;
+        }
+    
+        
+        if(end < right - left - 1){
+            start = left + 1;
+            end = right - left - 1;
+        }
+    }
+};
+
+
+int main(){
+    
+    string example("babad");
+    
+    Solution ans;
+    
+    cout << "The output is " <<ans.longestPalindrome(example) << endl;
+
+    return 0;
+}
