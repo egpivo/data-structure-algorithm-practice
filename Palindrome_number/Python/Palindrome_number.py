08e71eb89de5679288317f53fb663de214305e87
Wen-Ting Wang <egpivo@gmail.com>
2018-11-30 10:49:21 +0800

    Add a new leetocde problem

---
+++ Palindrome_number/Python/Palindrome_number.py
@@ -0,0 +1,26 @@
+class Solution:
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if x < 0 or (x != 0 and x % 10 == 0):
+            return False
+
+        rev = 0
+        while x > rev:
+            rev = 10 * rev + x % 10
+            x //= 10
+
+        return x == rev or x == rev // 10
+
+if __name__ == "__main__":
+  ans = Solution()
+
+  x = 123
+  print("input: {}".format(x))
+  print("Palindrome: {}".format(ans.isPalindrome(x)))
+
+  x = 121
+  print("input: {}".format(x))
+  print("Palindrome: {}".format(ans.isPalindrome(x)))
---
+++ Palindrome_number/c++/Palindrome_number.cpp
@@ -0,0 +1,8 @@
+//
+//  Palindrome_number.cpp
+//  
+//
+//  Created by Wen-Ting Wang on 2018/11/30.
+//
+
+#include "Palindrome_number.hpp"
