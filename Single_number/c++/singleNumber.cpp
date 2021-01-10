12cffef7e186f3dcbb7da5302a7f115bb805896e
egpivo <egpivo@gmail.com>
2021-01-11 07:51:46 +0800

    CHG: add a new solution; analyze the complexity

--- Single_number/c++/singleNumber.cpp
+++ Single_number/c++/singleNumber.cpp
@@ -1,7 +1,9 @@
 //
 //  singleNumber.cpp
+//  Note: `g++ -std=c++17 -g singleNumber.cpp -o singleNumber`
 //
 //  Created by Wen-Ting Wang on 2021/01/09.
+//
 //
 
 #include<iostream>
@@ -12,13 +14,13 @@
 #include<vector>
 using namespace std;
 
-class Solution {
+class Solution1 {
 public:
     int singleNumber(vector<int>& nums) {
         if(nums.size() == 1) return nums[0];
 
         unordered_map<int, int> numCount;
-        for(int i = 0; i < nums.size(); ++i){
+        for(int i = 0; i < nums.size(); ++i) {
             if(numCount.find(nums[i]) == numCount.end()){
                 numCount[nums[i]] = 1;
             }
@@ -27,7 +29,7 @@ public:
             }
         }
         
-        for(auto iter = numCount.begin(); iter != numCount.end(); ++iter){
+        for(auto iter = numCount.begin(); iter != numCount.end(); ++iter) {
            if (iter->second == 1) {
                 return iter->first;
             }
@@ -36,6 +38,26 @@ public:
     }
 };
 
+class Solution2 {
+public:
+    int singleNumber(vector<int>& nums) {
+        if(nums.size() == 1) return nums[0];
+
+        unordered_set<int> numSet;
+        for(int i = 0; i < nums.size(); ++i) {
+            if(numSet.find(nums[i]) == numSet.end()) {
+                numSet.insert(nums[i]);
+            }
+            else {
+                numSet.erase(nums[i]);
+            }
+        }
+        // return the first element in the container
+        return *(numSet.begin());
+    }
+};
+
+
 int main()
 {
     vector<int> numList;
@@ -43,9 +65,10 @@ int main()
     numList.push_back(2);
     numList.push_back(1);
 
-    Solution ans;
-
-    cout << ans.singleNumber(numList) << endl;
-    
+    Solution1 ans1;
+    Solution2 ans2;
+    cout << "True Anwser: 1" << endl;
+    cout << "Solution1: " << ans1.singleNumber(numList) << endl;
+    cout << "Solution2: " << ans2.singleNumber(numList) << endl;
     return 0;
 }
