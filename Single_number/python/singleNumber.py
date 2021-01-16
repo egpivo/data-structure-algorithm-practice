737b97c8a74665c2e8c6cac4bc514bad33bae048
egpivo <egpivo@gmail.com>
2021-01-16 12:45:34 +0800

    CHG: add an xor-based method

--- Single_number/python/singleNumber.py
+++ Single_number/python/singleNumber.py
@@ -7,7 +7,19 @@ class SingleNumber:
         return sorted(counter, key=counter.get, reverse=False)[0]
 
 
+class SingleNumberByXOR:
+    def single_number(self, nums: List[int]) -> int:
+        result = 0
+        for num in nums:
+          result ^= num
+        return result
+        
+
 if __name__ == "__main__":
   nums = [4,1,2,1,2]
   instance = SingleNumber()
-  print(instance.single_number(nums) == 4)
+  instance_xor = SingleNumberByXOR()
+
+  print("Test answer")
+  assert instance.single_number(nums) == 4, "Wrong Answer."
+  assert instance_xor.single_number(nums) == 4, "Wrong Answer."
