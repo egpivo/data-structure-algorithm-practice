71dc8abc7ed929bcf2d37d538b13c620420d2f87
Wen-Ting Wang <egpivo@gmail.com>
2021-08-25 15:37:11 +0800

    CHG: reformat codebase

--- mergeTwoSortedLinkedLists/python/Merge_two_sorted_lists.py
+++ mergeTwoSortedLinkedLists/python/Merge_two_sorted_lists.py
@@ -1,20 +1,25 @@
 # Definition for singly-linked list.
+from typing import Optional
+
+
 class ListNode:
     def __init__(self, x):
-      self.val = x
-      self.next = None
+        self.val = x
+        self.next = None
+
 
 class Solution:
-    def mergeTwoLists(self, l1, l2):
-        """
-        :type l1: ListNode
-        :type l2: ListNode
-        :rtype: ListNode
-        """
-        if(l1 == None): return l2
-        if(l2 == None): return l1
+    def mergeTwoLists(
+        self, l1: Optional[ListNode], l2: Optional[ListNode]
+    ) -> Optional[ListNode]:
+        if not l1 and not l2:
+            return None
+        if l1 is None:
+            return l2
+        if l2 is None:
+            return l1
 
-        if(l1.val < l2.val):
+        if l1.val < l2.val:
             l1.next = self.mergeTwoLists(l1.next, l2)
             return l1
         else:
@@ -22,19 +27,19 @@ class Solution:
             return l2
 
 
-
 if __name__ == "__main__":
-  l1 = ListNode(1)
-  l1.next = ListNode(2)
-  l1.next.next = ListNode(4)
+    l1 = ListNode(1)
+    l1.next = ListNode(2)
+    l1.next.next = ListNode(4)
 
-  l2 = ListNode(1)
-  l2.next = ListNode(3)
-  l2.next.next = ListNode(4)
+    l2 = ListNode(1)
+    l2.next = ListNode(3)
+    l2.next.next = ListNode(4)
 
-  ans = Solution()
-  newlist = ans.mergeTwoLists(l1, l2)
+    ans = Solution()
+    newlist = ans.mergeTwoLists(l1, l2)
 
-  for i in range(6):
-    print("%d \t" % newlist.val)
-    newlist = newlist.next
+    for i in range(6):
+        print("%d \t" % newlist.val)
+        newlist = newlist.next
+
