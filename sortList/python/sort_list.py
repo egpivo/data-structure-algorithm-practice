c79d4517275b7ea8cf49cf853093fe920e8b29d3
Wen-Ting Wang <egpivo@gmail.com>
2021-09-07 22:04:49 +0800

    CHG: add a new problem sort list by brute force way

---
+++ sortList/python/sort_list.py
@@ -0,0 +1,56 @@
+from typing import ForwardRef, Optional
+
+# Definition
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+      """ brute force"""
+      if head is None:
+          return None
+
+      all_values = []
+      while head:
+        all_values.append(head.val)
+        head = head.next
+
+      all_values.sort()
+      answer = ListNode()
+      forward = answer
+
+      for value in all_values:
+          forward.next = ListNode(value)
+          forward = forward.next
+      
+      return answer.next
+
+      
+def convert_to_list(head: ListNode) -> list:
+    output = []
+    while head:
+        output.append(head.val)
+        head = head.next
+    return output
+
+
+def convert_to_linked_list(input: list) -> ListNode:
+    if len(input) == 0:
+        return None
+
+    head = ListNode(input[0])
+    if len(input) > 1:
+        input = input[1:]
+        head.next = convert_to_linked_list(input)
+    return head
+
+
+if __name__ == "__main__":
+    input = [1, 2, 5, 3, 6, 4]
+    reversed = Solution().sortList(convert_to_linked_list(input))
+    print(convert_to_list(reversed))
+
+
