7afd0dc0d76abb59a2783f08572052587a5169fc
Wen-Ting Wang <egpivo@gmail.com>
2021-09-15 10:36:11 +0800

    CHG: add two different solutions for lengthOfLongestSubstring

--- longestSubstringWithoutRepeatingCharacters/python/longest_substring_without_repeating_characters.py
+++ longestSubstringWithoutRepeatingCharacters/python/longest_substring_without_repeating_characters.py
@@ -1,3 +1,45 @@
+class SolutionBruteForce:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        def is_duplicate(start: int, end: int) -> bool:
+            chars = [0] * 128
+            for i in range(start, end + 1):
+                c = s[i]
+                chars[ord(c)] += 1
+                if chars[ord(c)] > 1:
+                    return True
+            return False
+
+        n = len(s)
+        res = 0
+        for i in range(n):
+            for j in range(i, n):
+                if not is_duplicate(i, j):
+                    res = max(res, j - i + 1)
+        return res
+
+class SolutionSlidingWindow:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        left = 0
+        right = 0
+        
+        chars = [0] * 128
+        max_len = len(s)
+        ans = 0
+        while right < max_len:
+            r = s[right]
+            chars[ord(r)] += 1
+            
+            while chars[ord(r)] > 1:
+                l = s[left]
+                chars[ord(l)] -= 1
+                left += 1
+
+            ans = max(ans, right - left + 1)
+            right += 1
+        
+        return ans
+            
+
 class Solution:
     def lengthOfLongestSubstring(self, s):
         """
@@ -23,6 +65,17 @@ if __name__ == '__main__':
   test3 = "pwwkew"
 
   detect = Solution()
+  detect2 = SolutionBruteForce()
+  detect3 = SolutionSlidingWindow()
+
   print("Given %s  the length is %d"% (test1, detect.lengthOfLongestSubstring(test1)))
   print("Given %s  the length is %d"% (test2, detect.lengthOfLongestSubstring(test2)))
   print("Given %s  the length is %d"% (test3, detect.lengthOfLongestSubstring(test3)))
+
+  print("Given %s  the length is %d"% (test1, detect2.lengthOfLongestSubstring(test1)))
+  print("Given %s  the length is %d"% (test2, detect2.lengthOfLongestSubstring(test2)))
+  print("Given %s  the length is %d"% (test3, detect2.lengthOfLongestSubstring(test3)))
+
+  print("Given %s  the length is %d"% (test1, detect3.lengthOfLongestSubstring(test1)))
+  print("Given %s  the length is %d"% (test2, detect3.lengthOfLongestSubstring(test2)))
+  print("Given %s  the length is %d"% (test3, detect3.lengthOfLongestSubstring(test3)))
