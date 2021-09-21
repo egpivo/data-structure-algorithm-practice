4450fe1c75cf4715911bce3a3eba30480d6a142e
Wen-Ting Wang <egpivo@gmail.com>
2021-09-21 13:06:21 +0800

    CHG: add BFS solution

--- find_if_path_exists_in_graph/python/find_if_path_exists_in_graph.py
+++ find_if_path_exists_in_graph/python/find_if_path_exists_in_graph.py
@@ -1,5 +1,6 @@
-from collections import defaultdict
+from collections import defaultdict, deque
 from typing import List
+
 
 class DFS:
     def validPath(self, n: int, edges: List[List[int]], start: int, end: int) -> bool:
@@ -18,7 +19,7 @@ class DFS:
         def dfs(start):
             if start == end:
                 return True
-            
+
             marked.add(start)
 
             for x in collection[start]:
@@ -26,18 +27,44 @@ class DFS:
                     return True
 
             return False
+
         return dfs(start)
 
 
+class BFS:
+    def validPath(self, n: int, edges: List[List[int]], start: int, end: int) -> bool:
+        """
+        Time Complexity: O(V + E) ~ O(`n` + `n_edges`)
+        Space Complexity: O(V + E) ~ O(`n` + `n_edges`)
+        """
+        collection = defaultdict(list)
 
+        for x, y in edges:
+            collection[x].append(y)
+            collection[y].append(x)
+
+        q = deque([start])
+        marked = set([start])
+
+        while q:
+            node = q.popleft()
+            if node == end:
+                return True
+            for child_node in collection[node]:
+                if child_node not in marked:
+                    marked.add(child_node)
+                    q.append(child_node)
+        return False
 
 
 if __name__ == "__main__":
     n = 3
-    edges = [[0,1],[1,2],[2,0]]
+    edges = [[0, 1], [1, 2], [2, 0]]
     start = 0
     end = 2
-    result = DFS().validPath(n, edges, start, end)
-    print(result)
-
+    result_dfs = DFS().validPath(n, edges, start, end)
+    result_bfs = BFS().validPath(n, edges, start, end)
+    print(f"DFS: {result_dfs}")
+    print(f"BFS: {result_bfs}")
+    assert result_dfs == result_bfs, "Wrong results"
 
