0c0874ea53b890f77b125b6361a0a40bc98be0d8
Wen-Ting Wang <egpivo@gmail.com>
2021-09-23 22:14:40 +0800

    CHG: use quick find

--- numberOfProvinces/python/number_of_provinces.py
+++ numberOfProvinces/python/number_of_provinces.py
@@ -1,53 +1,42 @@
 from typing import Optional, List
 
-# Definition
 class UnionFind:
-    def __init__(self, size: int):
-        # used to check if the root is changed
-        self.root = [-1] * size
-        self.rank = [1] * size
-
+    """
+    Note
+    ----
+    - We can only use quick find instead of quick union
+    """
+    def __init__(self, size):
+        self.size = size
+        self.root = [i for i in range(size)]
+        
     def find(self, x):
-        if self.root[x] != -1:
-            return x
-        self.root[x] = self.find(self.root[x])
         return self.root[x]
-
+        
     def union(self, x, y):
         root_x = self.find(x)
         root_y = self.find(y)
-
+        
         if root_x != root_y:
-            if self.rank[x] >= self.rank[y]:
-                self.root[root_y] = root_x
-                self.rank[x] += 1
-            else:
-                self.root[root_x] = root_y
-                self.rank[y] += 1
-
-
+            for i in range(self.size):
+                if self.root[i] == root_y:
+                    self.root[i] = root_x
+        
+        
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        """
-        Time complexity: O(N^3)
-        space complexity: O(N)
-        """
-        size = len(isConnected)
-        uf = UnionFind(size)
-
-        for i in range(size):
-            for j in range(size):
+        n = len(isConnected)
+        uf = UnionFind(n)
+        
+        for i in range(n):
+            for j in range(i, n):
                 if i != j and isConnected[i][j] == 1:
                     uf.union(i, j)
+        return len(set(uf.root))
 
-        count = 0
-        for i in range(size):
-            if uf.root[i] == -1:
-                count += 1
-        return count
 
 if __name__ == "__main__":
-    isConnected = [[1,0,0],[0,1,0],[0,0,1]]
+    isConnected = [[1,1,0],[1,1,0],[0,0,1]]
     result = Solution().findCircleNum(isConnected)
     print(result)
 
