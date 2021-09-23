00d456bca483fe994560b0c3f3d083c407ed47fc
Wen-Ting Wang <egpivo@gmail.com>
2021-09-23 21:24:20 +0800

    CHG: add DFS method

--- numberOfIslands/python/number_of_islands.py
+++ numberOfIslands/python/number_of_islands.py
@@ -39,6 +39,28 @@ class SolutionUnionFind:
 
         return uf.max_count
 
+class SolutionDFS:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        nrow = len(grid)
+        ncol = len(grid[0])
+        def dfs(i, j):
+            if not ( 0 <= i < nrow) or not ( 0 <= j < ncol) or grid[i][j] != '1':
+                return
+            grid[i][j] = '#'
+            dfs(i, j + 1)
+            dfs(i, j - 1)
+            dfs(i - 1, j)
+            dfs(i + 1, j)
+            
+        count = 0
+        for i in range(nrow):
+            for j in range(ncol):
+                if grid[i][j] == '1':
+                    dfs(i, j)
+                    count += 1
+        return count
+
+
 if __name__ == "__main__":
     grid = [
         ["1","1","1","1","0"],
@@ -46,7 +68,7 @@ if __name__ == "__main__":
         ["1","1","0","0","0"],
         ["0","0","0","0","0"]
     ]
-    result = SolutionUnionFind().numIslands(grid)
-    print(result)
-
-
+    result_union_find = SolutionUnionFind().numIslands(grid)
+    result_dfs = SolutionDFS().numIslands(grid)
+    print(f"Union Find: {result_union_find}")
+    print(f"DFS: {result_dfs}")
