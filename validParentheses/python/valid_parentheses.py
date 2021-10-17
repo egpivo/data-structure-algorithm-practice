616a8b58721516882f755a62502eb7a16d10b0fb
egpivo <egpivo@gmail.com>
2021-10-17 12:17:08 +0800

    CHG: add a cleaner solution

--- validParentheses/python/valid_parentheses.py
+++ validParentheses/python/valid_parentheses.py
@@ -26,8 +26,34 @@ class Solution:
         return matched_pattern == remaining
             
 
+class CleanerSolution:
+    _char_mpa = {
+        "{": "}",
+        "(": ")",
+        "[": "]"
+    }
+
+    def isValid(self, s: str) -> bool:
+        if len(s) <= 1 or s[0] not in self._char_mpa or s[-1] in self._char_mpa:
+            return False
+
+        q = []
+        for char in s:
+            if char in self._char_mpa:
+                q.append(self._char_mpa[char])
+            else:
+                if not q:
+                    return False
+                stacked_char = q.pop()
+                if char != stacked_char:
+                    return False
+        
+        return len(q) == 0
+            
+
 
 if __name__ == "__main__":
   s = "()[]{}"
 
   print(Solution().isValid(s))
+  print(CleanerSolution().isValid(s))
