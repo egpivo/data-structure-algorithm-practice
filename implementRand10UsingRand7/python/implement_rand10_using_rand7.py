d743944ce20ed2603f14feb8c707458ab2a7e863
egpivo <egpivo@gmail.com>
2021-10-17 18:08:05 +0800

    CHG: add a python problem for generating random number by API

---
+++ implementRand10UsingRand7/python/implement_rand10_using_rand7.py
@@ -0,0 +1,17 @@
+# The rand7() API is already defined for you.
+# def rand7():
+# @return a random integer in the range 1 to 7
+
+class Solution:
+    def rand10(self):
+        """
+        Note
+        ----
+        - Rejection Sampling
+        """
+        index = acceptance = 40
+        while index >= acceptance:
+            row = rand7()
+            col = rand7()
+            index = (row - 1) * 7 + col - 1
+        return (index % 10) + 1
