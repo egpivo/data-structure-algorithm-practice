f54b8d49e3dfd9dc33f0afe8e281c2586fc7122c
Wen-Ting Wang <egpivo@gmail.com>
2021-10-26 21:50:46 +0800

    CHG: add a binary right side view question by dfs

---
+++ binaryTreeRightSideView/python/binary_tree_right_side_view.py
@@ -0,0 +1,44 @@
+from typing import Optional, List
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class DFSSolution:
+    """
+    Note
+    ----
+    - Time complexity: O(N)
+    - Space complexity: O(H)
+    """
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        if root is None:
+            return []
+
+        answer = []
+        def search_by_level(node: TreeNode, level: int) -> None:
+            if level == len(answer):
+                answer.append(node.val)
+
+            for child in (node.right, node.left):
+                if child:
+                    search_by_level(child, level + 1)
+
+        search_by_level(root, 0)
+        return answer
+
+
+
+
+if __name__ == "__main__":
+    tree = TreeNode(1)
+    tree.right = TreeNode(2)
+    tree.right.left = TreeNode(3)
+    tree.right.right = TreeNode(5)
+
+    print(f"The answer is {DFSSolution().rightSideView(tree)}")
+
