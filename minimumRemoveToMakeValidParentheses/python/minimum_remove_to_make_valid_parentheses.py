da580c009a53e6c88ac3f0fb49ef0ad933111f51
egpivo <egpivo@gmailcom>
2021-11-08 07:13:30 +0800

    CHG: add a new solution of two-pass to reduce redundant calculation

--- minimumRemoveToMakeValidParentheses/python/minimum_remove_to_make_valid_parentheses.py
+++ minimumRemoveToMakeValidParentheses/python/minimum_remove_to_make_valid_parentheses.py
@@ -34,6 +34,50 @@ class Solution:
         return "".join(answer)
 
 
+class TowPassSolution:
+
+    def minRemoveToMakeValid(self, s: str) -> str:
+        """ 2-pass
+
+        Note
+        ----
+        - Time complexity: O(n)
+        - Space complexity: O(n)
+        """
+
+        if not ("(" in s or ")" in s):
+            return s
+
+        # Pass 1: remove all invalid ")"
+        first_pass_chars = []
+        balance = 0
+        open_seen = 0
+
+        for char in s:
+            if char == "(":
+                balance += 1
+                open_seen += 1
+            if char == ")":
+                if balance == 0:
+                    continue
+                balance -= 1
+
+            first_pass_chars.append(char)
+
+        # Pass 2: remove the rightmost "("
+        result = []
+        open_to_keep = open_seen - balance
+        for char in first_pass_chars:
+            if char == "(":
+                open_to_keep -= 1
+                if open_to_keep < 0:
+                    continue
+            result.append(char)
+
+        return "".join(result)
+
+
 if __name__ == "__main__":
     s = "lee(t(c)o)de)"
     print(f"Answer: {Solution().minRemoveToMakeValid(s)}")
+    print(f"Answer: {TowPassSolution().minRemoveToMakeValid(s)}")
