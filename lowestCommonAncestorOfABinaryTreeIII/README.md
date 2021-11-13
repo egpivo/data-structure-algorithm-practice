4dc5c83b3c669f9b79fe1a4d5158457f7decd610
egpivo <egpivo@gmailcom>
2021-11-13 08:00:11 +0800

    CHG: add a new problem for calculating lowest common ancestor of binary tree

--- lowestCommonAncestorOfABinaryTreeIII/README.md
+++ lowestCommonAncestorOfABinaryTreeIII/README.md
@@ -1,13 +1,24 @@
-## [Range Sum of BST](https://leetcode.com/problems/range-sum-of-bst/)
+## [Lowest Common Ancestor of a Binary Tree III](https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree-iii/)
 
-Given the `root` node of a binary search tree and two integers `low` and `high`, return the sum of values of all nodes with a value in the inclusive range `[low, high]`.
+Given two nodes of a binary tree `p` and `q`, return their *lowest common ancestor* (LCA).
 
+Each node will have a reference to its parent node. The definition for `Node` is below:
+```
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node parent;
+}
+```
+According to the definition of LCA on Wikipedia: 
+> "The lowest common ancestor of two nodes p and q in a tree T is the lowest node that has both p and q as descendants (where we allow a node to be a descendant of itself)."
 
- 
 
 
 - Example 1:
 ```
-Input: root = [10,5,15,3,7,null,18], low = 7, high = 15
-Output: 32
-Explanation: Nodes 7, 10, and 15 are in the range [7, 15]. 7 + 10 + 15 = 32.```
+Input: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 1
+Output: 3
+Explanation: The LCA of nodes 5 and 1 is 3.
+```
--- lowestCommonAncestorOfABinaryTreeIII/python/lowest_common_ancestor_of_a_binary_tree_III.py
+++ lowestCommonAncestorOfABinaryTreeIII/python/lowest_common_ancestor_of_a_binary_tree_III.py
@@ -1,82 +1,63 @@
 from typing import Optional
 
 
-# Definition for a binary tree node.
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
+# Definition for a Node.
+class Node:
+    def __init__(self, val):
         self.val = val
-        self.left = left
-        self.right = right
+        self.left = None
+        self.right = None
+        self.parent = None
 
 
-class SolutionIterative:
-    """
-    Note
-    ----
-    - N: # nodes
-    - TC: O(N)
-    - SC: O(N)
-    """
+class Solution:
+    def lowestCommonAncestor(self, p: 'Node', q: 'Node') -> 'Node':
+        """
+        Example
+        -------
+        1. p = 5; q = 1 --> return 3
+        --> find the root; if p.parent is None: return root
+        --> from p.value (a node can be a descendant of itself)
 
-    def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
-        if root is None:
-            return 0
-        answer = 0
-        stack = [root]
-        while stack:
-            node = stack.pop()
-            if node:
-                if low <= node.val <= high:
-                    answer += node.val
-                if low < node.val:
-                    stack.append(node.left)
-                if high > node.val:
-                    stack.append(node.right)
-        return answer
+        5 --> 3
+        1 --> 3
 
+         Complexity
+        ----------
+        p: # of nodes
+        TC: O(p)
+        SC: O(1)
+        """
+        p1, q1 = p, q
 
-class SolutionDFSRecursive:
-    def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
-        self.answer = 0
-
-        def dfs(node):
-            if node:
-                if low <= node.val <= high:
-                    self.answer += node.val
-                if low < node.val:
-                    dfs(node.left)
-                if high > node.val:
-                    dfs(node.right)
-
-        dfs(root)
-        return self.answer
+        while p1 != q1:
+            if p1.parent is None:
+                p1 = q
+            else:
+                p1 = p1.parent
+            if q1.parent is None:
+                q1 = p1
+            else:
+                q1 = q1.parent
+        return p1
 
 
-class SolutionDFSRecursive2:
-    """This is thread-safe version"""
-    def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
-        if root is None:
-            return 0
-        else:
-            total = 0
-            if low <= root.val <= high:
-                total += root.val
-            if low < root.val:
-                total += self.rangeSumBST(root.left, low, high)
-            if high > root.val:
-                total += self.rangeSumBST(root.right, low, high)
-        return total
+class SolutionHashSet:
+    def lowestCommonAncestor(self, p: 'Node', q: 'Node') -> 'Node':
+        """
+        Complexity
+        ----------
+        p: # of nodes
+        TC: O(p)
+        SC: O(p)
+        """
+        hash_set = set()
 
-
-if __name__ == "__main__":
-    root = TreeNode(10)
-    root.left = TreeNode(5)
-    root.right = TreeNode(15)
-    root.left.left = TreeNode(3)
-    root.left.right = TreeNode(7)
-    root.right.right = TreeNode(18)
-    low = 7
-    high = 15
-    print(SolutionIterative().rangeSumBST(root, low, high))
-    print(SolutionDFSRecursive().rangeSumBST(root, low, high))
-    print(SolutionDFSRecursive2().rangeSumBST(root, low, high))
+        while p:
+            hash_set.add(p)
+            p = p.parent
+        while q:
+            if q in hash_set:
+                return q
+            q = q.parent
+        return None
