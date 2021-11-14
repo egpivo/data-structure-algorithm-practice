e2e7269ba30cb4af00cbdafa576119aab4267f72
egpivo <egpivo@gmailcom>
2021-11-15 07:44:20 +0800

    CHG: add a new problem for aggregate the node values columnwise

--- binaryTreeVerticalOrderTraversal/README.md
+++ binaryTreeVerticalOrderTraversal/README.md
@@ -1,18 +1,14 @@
-## [Convert Binary Search Tree to Sorted Doubly Linked List](https://leetcode.com/problems/convert-binary-search-tree-to-sorted-doubly-linked-list/)
+## [Binary Tree Vertical Order Traversal](https://leetcode.com/problems/binary-tree-vertical-order-traversal/)
 
-Convert a **Binary Search Tree** to a sorted **Circular Doubly-Linked List** in place.
+Given the `root` of a binary tree, return ***the vertical order traversal*** of its nodes' values. (i.e., from top to bottom, column by column).
 
-You can think of the left and right pointers as synonymous to the predecessor and successor pointers in a doubly-linked list. For a circular doubly linked list, the predecessor of the first element is the last element, and the successor of the last element is the first element.
+If two nodes are in the same row and column, the order should be from ***left to right***.
 
-We want to do the transformation **in place**. After the transformation, the left pointer of the tree node should point to its predecessor, and the right pointer should point to its successor. You should return the pointer to the smallest element of the linked list.
-
-According to the definition of LCA on Wikipedia: 
-> "The lowest common ancestor of two nodes p and q in a tree T is the lowest node that has both p and q as descendants (where we allow a node to be a descendant of itself)."
 
 
 
 - Example 1:
 ```
-Input: root = [4,2,5,1,3]
-Output: [1,2,3,4,5]
+Input: root = [3,9,20,null,null,15,7]
+Output: [[9],[3,15],[20],[7]]
 ```
--- binaryTreeVerticalOrderTraversal/python/binary_tree_vertical_order_traversal.py
+++ binaryTreeVerticalOrderTraversal/python/binary_tree_vertical_order_traversal.py
@@ -1,13 +1,43 @@
-# Definition for a Node.
-class Node:
-    def __init__(self, val, left=None, right=None):
+from typing import Optional, List
+from collections import defaultdict, deque
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
 
 
-class Solution:
-    def treeToDoublyList(self, root: 'Node') -> 'Node':
+class SolutionBFS:
+    def verticalOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        """
+        Complexity
+        ---------
+        - N = # of nodes
+        - TC: O(NlogN) (due to the sort with the worst case)
+        - SC: O(N)
+        """
+        if root is None:
+            return []
+        column_table = defaultdict(list)
+        # collect (node, column position)
+        queue = deque([(root, 0)])
+
+        while queue:
+            node, column = queue.popleft()
+
+            if node is not None:
+                column_table[column].append(node.val)
+                queue.append((node.left, column - 1))
+                queue.append((node.right, column + 1))
+
+        return [column_table[x] for x in sorted(column_table.keys())]
+
+
+class SolutionBFSWithoutSorting:
+    def verticalOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
         """
         Complexity
         ---------
@@ -16,28 +46,69 @@ class Solution:
         - SC: O(p)
         """
         if root is None:
-            return None
+            return []
 
-        head, tail = None, None
+        column_table = defaultdict(list)
+        # collect (node, column position)
+        queue = deque([(root, 0)])
+        min_column = max_column = 0
+        while queue:
+            node, column = queue.popleft()
 
-        def dfs(node):
-            nonlocal head, tail
-            # base condition
-            if node is None:
-                return None
+            if node is not None:
+                column_table[column].append(node.val)
+                min_column = min(min_column, column)
+                max_column = max(max_column, column)
+                queue.append((node.left, column - 1))
+                queue.append((node.right, column + 1))
 
-            dfs(node.left)
-            if head is None:
-                head = node
-            else:
-                tail.right = node
-                node.left = tail
-            tail = node
-            dfs(node.right)
+        return [column_table[x] for x in range(min_column, max_column + 1)]
 
-        dfs(root)
 
-        head.left = tail
-        tail.right = head
+class SolutionDFS:
+    def verticalOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        """
+        Complexity
+        ---------
+        - W = width of the tree
+        - H = height of the tree
+        - p = # nodes of the tree
+        - TC: O(W⋅HlogH)
+        - SC: O(p)
+        """
+        if root is None:
+            return []
 
-        return head
+        column_table = defaultdict(list)
+        # collect (node, column position)
+        min_column = max_column = 0
+
+        def dfs(node, row, column):
+            nonlocal min_column, max_column
+            if node is not None:
+                column_table[column].append((row, node.val))
+                min_column = min(min_column, column)
+                max_column = max(max_column, column)
+
+                dfs(node.left, row + 1, column - 1)
+                dfs(node.right, row + 1, column + 1)
+
+        dfs(root, 0, 0)
+        result = []
+        for column in range(min_column, max_column + 1):
+            # sort by row
+            column_table[column].sort(key=lambda x: x[0])
+            column_vals = [val for _, val in column_table[column]]
+            result.append(column_vals)
+        return result
+
+
+if __name__ == "__main__":
+    tree = TreeNode(3)
+    tree.left = TreeNode(9)
+    tree.right = TreeNode(20)
+    tree.right.left = TreeNode(15)
+    tree.right.left = TreeNode(7)
+    print(f"The answer is {SolutionBFS().verticalOrder(tree)}")
+    print(f"The answer is {SolutionBFSWithoutSorting().verticalOrder(tree)}")
+    print(f"The answer is {SolutionDFS().verticalOrder(tree)}")
