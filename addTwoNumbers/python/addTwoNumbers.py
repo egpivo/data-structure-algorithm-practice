7f403ff08c36aca15e78d356897550589323c316
egpivo <egpivo@gmailcom>
2021-11-18 22:05:01 +0800

    CHG: modify the solution

--- addTwoNumbers/python/addTwoNumbers.py
+++ addTwoNumbers/python/addTwoNumbers.py
@@ -15,12 +15,12 @@ class Solution:
       temp = ls
       rest = 0
 
-      while(l1 != None or  l2 != None or rest):
-        sum = (l1.val if l1!= None else 0) + (l2.val if l2 != None else 0) + rest
+      while(l1 or  l2  or rest):
+        sum = (l1.val if l1 else 0) + (l2.val if l2 else 0) + rest
         rest = 1 if sum >= 10 else 0
         temp.next = ListNode(sum%10)
-        l1 = l1.next if l1!= None else None
-        l2 = l2.next if l1!= None else None
+        l1 = l1.next if l1 else None
+        l2 = l2.next if l2 else None
         temp = temp.next
 
       return ls.next
