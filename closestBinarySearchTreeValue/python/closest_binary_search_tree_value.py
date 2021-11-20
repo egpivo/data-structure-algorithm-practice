98b8f72118661c4bd03c0540a3c4e7483add9e22
egpivo <egpivo@gmailcom>
2021-11-20 10:51:16 +0800

    CHG: add an optimized solution

--- closestBinarySearchTreeValue/python/closest_binary_search_tree_value.py
+++ closestBinarySearchTreeValue/python/closest_binary_search_tree_value.py
@@ -22,10 +22,41 @@ class Solution:
         TC: O(N)
         SC: O(N) (inorder traversal)
         """
+
         def inorder(node):
             return inorder(node.left) + [node.val] + inorder(node.right) if node else []
 
         return min(inorder(root), key=lambda x: abs(target - x))
+
+
+class OpimizedSolution:
+    def closestValue(self, root: Optional[TreeNode], target: float) -> int:
+        """
+        Complexity
+        ----------
+        - k: an index of closest element
+        - H: height
+        TC:
+          - avg O(k) (balance tree)
+          - worst case O(H + k)
+        SC: O(H)
+        """
+        stack = []
+        min_val = float('-inf')
+
+        while stack or root:
+            while root:
+                stack.append(root)
+                root = root.left
+            root = stack.pop()
+
+            if min_val <= target <= root.val:
+                return min(min_val, root.val, key=lambda x: abs(target - x))
+
+            min_val = root.val
+            root = root.right
+
+        return min_val
 
 
 if __name__ == "__main__":
@@ -36,3 +67,4 @@ if __name__ == "__main__":
     tree.right = TreeNode(5)
     target = 3.714286
     print(f"The answer is {Solution().closestValue(tree, target)}")
+    print(f"The optimized answer is {OpimizedSolution().closestValue(tree, target)}")
