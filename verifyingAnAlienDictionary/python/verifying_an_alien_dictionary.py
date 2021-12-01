0447ab17536d6bd1fe5b4b82006f182441b9850a
egpivo <egpivo@gmail.com>
2021-12-01 08:13:18 +0800

    CHG: add a new problem for verifying an alien dictionary

---
+++ verifyingAnAlienDictionary/python/verifying_an_alien_dictionary.py
@@ -0,0 +1,36 @@
+from typing import List
+
+class Solution:
+    def isAlienSorted(self, words: List[str], order: str) -> bool:
+
+        """
+        Complexity
+        ----------
+        - M: # of characters in words
+        TC: O(M)
+        SC: O(1)
+        """
+        order_map = {char: idx for idx, char in enumerate(order)}
+
+        for i in range(len(words) - 1):
+            for j in range(len(words[i])):
+                if j >= len(words[i+1]):
+                    return False
+
+                if order_map[words[i][j]] != order_map[words[i+1][j]]:
+                    if order_map[words[i][j]] > order_map[words[i+1][j]]:
+                        return False
+                    break
+        return True
+
+
+
+
+if __name__ == "__main__":
+    words = ["hello","leetcode"]
+    order = "hlabcdefgijkmnopqrstuvwxyz"
+    print(f"Solution: {Solution().isAlienSorted(words, order)}")
+
+    words = ["apple", "app"]
+    order = "abcdefghijklmnopqrstuvwxyz"
+    print(f"Solution: {Solution().isAlienSorted(words, order)}")
