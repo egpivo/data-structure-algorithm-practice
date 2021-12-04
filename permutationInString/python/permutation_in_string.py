25451e85476b08708f4871c0cc7277678105dc3b
egpivo <egpivo@gmail.com>
2021-12-04 08:37:45 +0800

    CHG: refactor codebase

--- permutationInString/python/permutation_in_string.py
+++ permutationInString/python/permutation_in_string.py
@@ -11,24 +11,30 @@ class Solution:
     """
 
     def checkInclusion(self, s1: str, s2: str) -> bool:
+        n1 = len(s1)
+        n2 = len(s2)
         s1_freq = [0] * 26
         for char in s1:
-            s1_freq[ord(char) - ord('a')] += 1
+            s1_freq[self.calculate_letter_index_from_a(char)] += 1
 
         s2_freq = [0] * 26
-        for lo in range(len(s2) - len(s1) + 1):
-            hi = lo + len(s1) - 1
+        for lo in range(n2 - n1 + 1):
+            hi = lo + n1 - 1
             if lo == 0:
-                for k in range(len(s1)):
-                    s2_freq[ord(s2[k]) - ord('a')] += 1
+                for k in range(n1):
+                    s2_freq[self.calculate_letter_index_from_a(s2[k])] += 1
             else:
-                s2_freq[ord(s2[hi]) - ord('a')] += 1
+                s2_freq[self.calculate_letter_index_from_a(s2[hi])] += 1
 
             if s1_freq == s2_freq:
                 return True
             s2_freq[ord(s2[lo]) - ord('a')] -= 1
 
         return False
+
+    @staticmethod
+    def calculate_letter_index_from_a(lower_letter: str) -> int:
+        return ord(lower_letter) - ord('a')
 
 
 if __name__ == "__main__":
