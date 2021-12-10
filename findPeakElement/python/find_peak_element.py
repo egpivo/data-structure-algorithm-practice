7138112b04611749e3fba793b90109304b00d572
egpivo <egpivo@gmail.com>
2021-12-10 08:58:55 +0800

    CHG: add a recursive soltion

--- findPeakElement/python/find_peak_element.py
+++ findPeakElement/python/find_peak_element.py
@@ -9,6 +9,7 @@ class SolutionIterative:
     - TC: O(log_2 n)
     - SC: O(1)
     """
+
     def findPeakElement(self, nums: List[int]) -> int:
         len_nums = len(nums)
         left, right = 0, len_nums - 1
@@ -23,6 +24,28 @@ class SolutionIterative:
                 return mid
 
 
+class SolutionRecursive:
+    """
+
+    Complexity
+    ----------
+    - TC: O(log_2 n)
+    - SC: O(log_2 n)
+    """
+
+    def findPeakElement(self, nums: List[int]) -> int:
+        return self._recurse(nums, 0, len(nums) - 1)
+
+    def _recurse(self, nums: List[int], left: int, right: int) -> int:
+        if left == right:
+            return left
+        mid = left + (right - left) // 2
+        if nums[mid] > nums[mid + 1]:
+            return self._recurse(nums, left, mid)
+        return self._recurse(nums, mid + 1, right)
+
+
 if __name__ == "__main__":
     nums = [1, 2, 3, 1]
     print(f"Solution: {SolutionIterative().findPeakElement(nums)}")
+    print(f"Solution: {SolutionRecursive().findPeakElement(nums)}")
