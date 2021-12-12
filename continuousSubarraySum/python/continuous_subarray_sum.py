e6204700ec64cbed7284a0bbb216ab01845cb007
egpivo <egpivo@gmail.com>
2021-12-12 11:17:33 +0800

    CHG: add a new problem for checking the subarray sum

---
+++ continuousSubarraySum/python/continuous_subarray_sum.py
@@ -0,0 +1,33 @@
+from typing import List
+
+
+class Solution:
+    """
+
+    Complexity
+    ----------
+    - TC: O(n)
+    - SC: O(n)
+    """
+    def checkSubarraySum(self, nums: List[int], k: int) -> bool:
+        remainder_map = {0: -1}
+        total = 0
+
+        for idx, num in enumerate(nums):
+            total += num
+            total %= k
+
+            if total not in remainder_map:
+                remainder_map[total] = idx
+            else:
+                if idx - remainder_map[total] > 1:
+                    return True
+
+        return False
+
+
+if __name__ == "__main__":
+    nums = [23, 2, 6, 4, 7]
+
+    print(f"Solution: {Solution().checkSubarraySum(nums, 6)}")
+    print(f"Solution: {Solution().checkSubarraySum(nums, 9)}")
