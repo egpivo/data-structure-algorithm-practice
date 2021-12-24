f3aa9693adb6b16a53306b4f21da432878434f11
egpivo <egpivo@gmail.com>
2021-12-24 11:20:00 +0800

    CHG: add a new problem for LCA of a binary tree

---
+++ lowestCommonAncestorOfABinaryTree/python/lowest_common_ancestor_of_a_binary_tree.py
@@ -0,0 +1,54 @@
+# Definition for a Node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class SolutionRecursive:
+    """
+
+    Complexity
+    ----------
+    - TC: O(n)
+    - SC: O(n)
+    """
+    def lowestCommonAncestor(
+        self, root: "TreeNode", p: "TreeNode", q: "TreeNode"
+    ) -> "TreeNode":
+        ans = None
+
+        def recurse_tree(node):
+            nonlocal ans
+
+            if not node:
+                return False
+
+            left = recurse_tree(node.left)
+            right = recurse_tree(node.right)
+
+            mid = node == p or node == q
+            if mid + left + right >= 2:
+                ans = node
+            return mid or left or right
+
+        recurse_tree(root)
+        return ans
+
+
+if __name__ == "__main__":
+    p = TreeNode(5)
+    p.left = TreeNode(6)
+    p.right = TreeNode(2)
+    p.right.left = TreeNode(7)
+    p.right.right = TreeNode(4)
+
+    q = TreeNode(1)
+    q.left = TreeNode(0)
+    q.right = TreeNode(8)
+    node = TreeNode(3)
+    node.left = p
+    node.right = q
+
+    print(f"{SolutionRecursive().lowestCommonAncestor(node, p, q).val}")
