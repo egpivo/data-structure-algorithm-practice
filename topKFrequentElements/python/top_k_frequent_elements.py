afb3963e272dc70c25a656df65be65b6f46fc67f
egpivo <egpivo@gmail.com>
2022-01-03 09:15:25 +0800

    CHG: add a new solution by priority queue

--- topKFrequentElements/python/top_k_frequent_elements.py
+++ topKFrequentElements/python/top_k_frequent_elements.py
@@ -1,3 +1,4 @@
+import heapq
 from collections import Counter
 from typing import List
 
@@ -12,11 +13,20 @@ class Solution:
 
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         freq = Counter(nums)
-        sorted_freq = sorted(freq, key=lambda x: freq.get(x), reverse=True)
+        sorted_freq = sorted(freq, key=freq.get, reverse=True)
         return sorted_freq[:k]
+
+
+class SolutionHeap:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        if len(nums) <= k:
+            return nums
+        freq = Counter(nums)
+        return heapq.nlargest(k, freq.keys(), key=freq.get)
 
 
 if __name__ == "__main__":
     nums = [1, 1, 1, 2, 2, 3]
     k = 2
     print(f"{Solution().topKFrequent(nums, k)}")
+    print(f"{SolutionHeap().topKFrequent(nums, k)}")
