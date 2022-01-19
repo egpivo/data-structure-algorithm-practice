07ca067bef7c4c3a3b72aa5b7a597d863ef44995
egpivo <egpivo@gmail.com>
2022-01-19 19:05:08 +0800

    CHG: add a new problem for coloring border

---
+++ colorBorder/python/color_border.py
@@ -0,0 +1,48 @@
+from typing import List
+
+
+class SolutionDFS:
+    """
+    Complexity
+    ----------
+    - TC: O(mn)
+    - SC: O(mn)
+    """
+    directions = ((0, 1), (1, 0), (0, -1), (-1, 0))
+
+    def colorBorder(self, grid: List[List[int]], row: int, col: int, color: int) -> List[List[int]]:
+        nrows = len(grid)
+        ncols = len(grid[0])
+
+        seen = set()
+        border = set()
+
+        def dfs(x, y, color):
+            if (x, y) in seen:
+                return
+
+            seen.add((x, y))
+            is_on_border = False
+            for dx, dy in self.directions:
+                new_x, new_y = x + dx, y + dy
+                if 0 <= new_x < nrows and 0 <= new_y < ncols and grid[new_x][new_y] == color:
+                    dfs(new_x, new_y, color)
+                else:
+                    is_on_border = True
+
+            if is_on_border:
+                border.add((x, y))
+
+        dfs(row, col, grid[row][col])
+        for x, y in border:
+            grid[x][y] = color
+
+        return grid
+
+
+if __name__ == "__main__":
+    grid = [[1, 2, 2], [2, 3, 2]]
+    row = 0
+    col = 1
+    color = 3
+    print(f"Solution: {SolutionDFS().colorBorder(grid, row, col, color)}")
