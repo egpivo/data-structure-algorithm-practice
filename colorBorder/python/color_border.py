f3df593b054b98667e1abca8183bade47f0e1177
egpivo <egpivo@gmail.com>
2022-01-19 19:50:58 +0800

    CHG: add a bfs solution

--- colorBorder/python/color_border.py
+++ colorBorder/python/color_border.py
@@ -1,3 +1,4 @@
+from collections import deque
 from typing import List
 
 
@@ -40,9 +41,47 @@ class SolutionDFS:
         return grid
 
 
+class SolutionBFS:
+    """
+    Complexity
+    ----------
+    - TC: O(mn)
+    - SC: O(mn)
+    """
+    directions = ((0, 1), (1, 0), (0, -1), (-1, 0))
+
+    def colorBorder(self, grid: List[List[int]], row: int, col: int, color: int) -> List[List[int]]:
+        nrows = len(grid)
+        ncols = len(grid[0])
+        queue = deque([(row, col)])
+
+        seen = set()
+        border = set()
+
+        while queue:
+            x, y = queue.popleft()
+
+            if (x, y) in seen:
+                continue
+
+            seen.add((x, y))
+            for dx, dy in self.directions:
+                new_x, new_y = x + dx, y + dy
+                if not (0 <= new_x < nrows and 0 <= new_y < ncols and grid[new_x][new_y] == grid[x][y]):
+                    border.add((x, y))
+                    continue
+                if (new_x, new_y) not in queue:
+                    queue.append((new_x, new_y))
+
+        for x, y in border:
+            grid[x][y] = color
+        return grid
+
+
 if __name__ == "__main__":
     grid = [[1, 2, 2], [2, 3, 2]]
     row = 0
     col = 1
     color = 3
     print(f"Solution: {SolutionDFS().colorBorder(grid, row, col, color)}")
+    print(f"Solution: {SolutionBFS().colorBorder(grid, row, col, color)}")
