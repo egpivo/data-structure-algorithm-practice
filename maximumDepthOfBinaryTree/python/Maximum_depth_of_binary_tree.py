491cf1638ebc32aee27b5c50af53efadb6621112
Wen-Ting Wang <egpivo@gmail.com>
2022-02-14 20:20:51 +0800

    CHG: add a bfs solution

--- maximumDepthOfBinaryTree/python/Maximum_depth_of_binary_tree.py
+++ maximumDepthOfBinaryTree/python/Maximum_depth_of_binary_tree.py
@@ -1,9 +1,12 @@
+from collections import deque
+
 # Definition for a binary tree node.
 class TreeNode:
-  def __init__(self, x):
-    self.val = x
-    self.left = None
-    self.right = None
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
 
 class Solution:
     def maxDepth(self, root):
@@ -11,17 +14,46 @@ class Solution:
         :type root: TreeNode
         :rtype: int
         """
-        if root is None: return 0
+        if root is None:
+            return 0
 
         return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
 
 
-if __name__ == "__main__":
-  tree = TreeNode(3)
-  tree.left = TreeNode(9)
-  tree.right = TreeNode(20)
-  tree.right.left = TreeNode(15)
-  tree.right.right = TreeNode(7)
+class SolutionBFS:
+    def maxDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if root is None:
+            return 0
 
-  ans = Solution()
-  print(ans.maxDepth(tree))
+        q = deque([root])
+        count = 0
+        while q:
+            level = len(q)
+
+            for _ in range(level):
+                node = q.popleft()
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            count += 1
+
+        return count
+
+
+if __name__ == "__main__":
+    tree = TreeNode(3)
+    tree.left = TreeNode(9)
+    tree.right = TreeNode(20)
+    tree.right.left = TreeNode(15)
+    tree.right.right = TreeNode(7)
+
+    ans = Solution()
+    print(ans.maxDepth(tree))
+    print(SolutionBFS().maxDepth(tree))
+
+    
