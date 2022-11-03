7c83dfe29a42e4bcde57cbbf2d3b1a914e5a202b
Wen-Ting Wang <egpivo@gmail.com>
2022-11-03 14:14:01 +0800

    CHG: add two problems - combination sum and find-first-and-last-position

---
+++ problems/combinationSum/python/combination_sum.py
@@ -0,0 +1,58 @@
+from typing import List
+
+
+class BackTrackingSolution:
+    """ Backtracking
+
+    Complexity
+    ----------
+    - TC: O(n^(target/min(candidates)))
+       - n = len(candidates)
+    - SC: O(target/min(candidates))
+    """
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        answer = []
+        n = len(candidates)
+
+        def dfs(collection, idx):
+            if sum(collection) > target:
+                return
+            elif sum(collection) == target:
+                answer.append(collection)
+                return
+            
+            for i in range(idx, n):
+                dfs(collection + [candidates[i]], i)
+
+        dfs([], 0)
+        return answer
+
+class DpSolution:
+    """ Dynamic Programming
+
+    Complexity
+    ----------
+    - TC: O(n^(target/min(candidates)))
+       - n = len(candidates)
+    - SC: O(target ))
+    """
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        dp = [[] for _ in range(target + 1)]
+        for candidate in candidates:
+            for c in range(candidate, target + 1):
+                if candidate == c:
+                    dp[c].append([candidate])
+
+                for cumulative in dp[c - candidate]:
+                    dp[c].append(cumulative + [candidate])
+
+        return dp[-1]
+
+
+if __name__ == "__main__":
+    candidates = [2,3,6,7]
+    target = 7
+
+    print(f"Solution: {BackTrackingSolution().combinationSum(candidates, target)}")
+    print(f"Solution: {DpSolution().combinationSum(candidates, target)}")
+
