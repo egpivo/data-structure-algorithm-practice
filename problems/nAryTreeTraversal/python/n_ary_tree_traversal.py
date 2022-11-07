8309ff096bacd353a98177d08acb289226fd2b1d
Wen-Ting Wang <egpivo@gmail.com>
2022-11-07 15:49:35 +0800

    CHG: add preorder, postorder and levelorder

--- problems/nAryTreeTraversal/python/n_ary_tree_traversal.py
+++ problems/nAryTreeTraversal/python/n_ary_tree_traversal.py
@@ -1,32 +1,60 @@
-from collections import defaultdict
+
+from this import d
 from typing import List
 
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+
+
+
 class Solution:
-    """
-    Note
-    ----
-    - TC: O(n)
-    - SC: O(n)
-    """
-    def compute_longest_length(self, array: List[int], k: int) -> int:
-        hash_map = defaultdict(int)
-        prefix_sum = 0
-        answer = 0
-
-        for idx, num in enumerate(array):
-            prefix_sum += num
-
-            if prefix_sum == k:
-                answer = idx + 1
-            elif prefix_sum - k in hash_map:
-                answer = max(answer, idx - hash_map[prefix_sum - k])
-            if prefix_sum not in hash_map:
-                hash_map[prefix_sum] = idx
-
+    def preorder(self, root: 'Node') -> List[int]:
+        def dfs(node):
+            if not node:
+                return None
+            
+            answer.append(node.val)
+            for child in node.children:
+                dfs(child)
+        
+        answer = []
+        dfs(root)
         return answer
 
+    def postorder(self, root: 'Node') -> List[int]:
+        def dfs(node):
+            if not node:
+                return None
+            
+            for child in node.children:
+                dfs(child)
+            answer.append(node.val)
+        
+        answer = []
+        dfs(root)
+        return answer
 
-if __name__ == "__main__":
-    array = [10, 5, 2, 7, 1, 9]
-    k = 15
-    print(f"{Solution().compute_longest_length(array, k)}")
+    def levelOrder(self, root: 'Node') -> List[List[int]]:
+        queue = [root]
+        answer = []
+
+        if not root:
+            return answer
+
+        while queue:
+            level = len(queue)
+            level_vals = []
+            for _ in range(level):
+                node = queue.pop(0)
+                
+                if node.val is not None:
+                    level_vals.append(node.val)
+                for child in node.children:
+                    queue.append(child)
+            
+            answer.append(level_vals)
+            
+        return answer
