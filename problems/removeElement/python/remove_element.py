36a31c818f586dd86a32fe5bef50bc6792bf9f36
Wen-Ting Wang <egpivo@gmail.com>
2023-08-13 09:43:25 +0800

    CHG: add a new problem for removing elements

---
+++ problems/removeElement/python/remove_element.py
@@ -0,0 +1,24 @@
+from typing import List
+
+
+class Solution:
+    """
+    Complexity
+    ----------
+    - TC: O(n)
+    """
+    def removeElement(self, nums: List[int], val: int) -> int:
+        count = 0
+        
+        for num in nums:
+            if num != val:
+                nums[count] = num
+                count += 1
+        return count
+        
+
+
+if __name__ == "__main__":
+    nums = [3,2,2,3]
+    val = 3
+    print(f"k = {Solution().removeElement(nums, val)} \nnums = {nums}")
