90d8d082c1d466a9e567bdb0d2bbbfc09c381d7f
Wen-Ting Wang <egpivo@gmail.com>
2023-08-15 08:51:47 +0800

    CHG: add a new problem of removing duplicate elements in a sorted array

---
+++ problems/removeElementRemoveDuplicatesFromSortedArray/python/remove_duplicates_from_sorted_array.py
@@ -0,0 +1,32 @@
+from typing import List
+
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        """
+        Complexity
+        ----------
+        - TC: O(1)
+        """
+        count = 1
+        prev = nums[0]
+        left = 0
+        right = 1
+
+        while right < len(nums):
+            if nums[right] != prev:
+                nums[count] = nums[right]
+                count += 1
+                left = right
+            elif right - left <= 1:
+                nums[count] = nums[right]
+                count += 1
+            prev = nums[right]
+            right += 1
+        return count
+
+
+
+if __name__ == "__main__":
+    nums = [1,1,1,2,2,3]
+    print(f"{Solution().removeDuplicates(nums)} \nnums = {nums}")
