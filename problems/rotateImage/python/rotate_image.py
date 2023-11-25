7812bff09481f231a25dcbbe3023fdf1e5251f54
egpivo <egpivo@gmail.com>
2023-11-25 11:12:36 +0800

    FIX: correct typos

--- problems/rotateImage/python/rotate_image.py
+++ problems/rotateImage/python/rotate_image.py
@@ -10,14 +10,14 @@ class Solution:
     """
     def rotate(self, matrix: List[List[int]]) -> None:
         self.transpose(matrix)
-        self.relect(matrix)
+        self.reflect(matrix)
 
     def transpose(self, matrix: List[List[int]]) -> None:
         for i in range(len(matrix)):
             for j in range(i + 1, len(matrix[0])):
                 matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
 
-    def relect(self, matrix: List[List[int]]) -> None:
+    def reflect(self, matrix: List[List[int]]) -> None:
         for i in range(len(matrix)):
             for j in range(len(matrix[0]) // 2):
                 matrix[i][j], matrix[i][len(matrix[0]) - j - 1] = matrix[i][len(matrix[0]) - j - 1], matrix[i][j]
