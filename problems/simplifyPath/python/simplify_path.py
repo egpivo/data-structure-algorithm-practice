32e15608e2321b75b5902f37a25c976347596a41
egpivo <egpivo@gmail.com>
2023-11-30 09:28:57 +0800

    CHG: add a new problem to simplify the path

---
+++ problems/simplifyPath/python/simplify_path.py
@@ -0,0 +1,20 @@
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        path = path.replace("//", "/")
+        split_path = path.split("/")
+        directories = []
+        for directory in split_path:
+            if directories and directory == "..":
+                directories.pop(-1)
+            elif directory in (".", "", ".."):
+                continue
+            else:
+                directories.append(directory)
+
+        return f"/{'/'.join(directories)}"
+
+        
+        
+if __name__ == "__main__":
+    s = "/../"
+    print(Solution().simplifyPath(s))
