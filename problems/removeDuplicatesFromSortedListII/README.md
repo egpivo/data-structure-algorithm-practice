f64834d0cb39d74331788ecab174630b23266acb
egpivo <egpivo@gmail.com>
2023-12-02 20:59:23 +0800

    CHG: add a new problem for removing the duplicates from the linked list

---
+++ problems/removeDuplicatesFromSortedListII/README.md
@@ -0,0 +1,9 @@
+## [Remove Duplicates from Sorted List II](https://leetcode.com/problems/remove-duplicates-from-sorted-list-ii)
+Given the head of a sorted linked list, delete all nodes that have duplicate numbers, leaving only distinct numbers from the original list. Return the linked list sorted as well.
+
+
+- Example:
+```
+Input: head = [1,2,3,3,4,4,5]
+Output: [1,2,5]
+```
---
+++ problems/removeDuplicatesFromSortedListII/python/remove_duplicates_from_sorted_list_ii.py
@@ -0,0 +1,46 @@
+# Definition for singly-linked list.
+from typing import Optional
+
+
+class ListNode:
+  def __init__(self, x):
+       self.val = x
+       self.next = None
+
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head:
+            return head
+        init = ListNode(-1)
+        init.next = head
+
+        current = head
+        previous = init
+        while current:
+            while current.next and current.next.val == current.val:
+                current = current.next
+
+            if previous.next == current:
+                previous = previous.next
+                current = current.next
+            else:
+                previous.next = current.next
+                current = previous.next
+
+        return init.next
+
+
+
+if __name__ == "__main__":
+  data = ListNode(1)
+  data.next = ListNode(2)
+  data.next.next = ListNode(2)
+  data.next.next.next = ListNode(4)
+  data.next.next.next.next = ListNode(5)
+
+  ans = Solution()
+
+  answer_list = ans.deleteDuplicates(data)
+  while answer_list:
+    print("%d \t" % answer_list.val)
+    answer_list = answer_list.next
