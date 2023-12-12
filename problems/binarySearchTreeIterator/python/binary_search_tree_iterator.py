19b2475796bc582f08a84ddf2fff74e3bb3cb6fd
egpivo <egpivo@gmail.com>
2023-12-12 09:38:58 +0800

    CHG: add a stack method

--- problems/binarySearchTreeIterator/python/binary_search_tree_iterator.py
+++ problems/binarySearchTreeIterator/python/binary_search_tree_iterator.py
@@ -9,7 +9,7 @@ class TreeNode:
         self.right = right
 
 
-class BSTIterator:
+class BSTIteratorRecursive:
     """
     Notes
     ----
@@ -18,19 +18,57 @@ class BSTIterator:
     """
 
     def __init__(self, root: Optional[TreeNode]):
-        self.iterator = self._inorder(root)
-        self.next_value = next(self.iterator, None)
+        self.inorder_generator = self._generate_inorder_value(root)
+        self.next_value = next(self.inorder_generator, None)
 
-    def _inorder(self, node: Optional[TreeNode]) -> Generator[int, None, None]:
+    def _generate_inorder_value(
+        self, node: Optional[TreeNode]
+    ) -> Generator[int, None, None]:
         if node:
-            yield from self._inorder(node.left)
+            yield from self._generate_inorder_value(node.left)
             yield node.val
-            yield from self._inorder(node.right)
+            yield from self._generate_inorder_value(node.right)
+        else:
+            self.next_value = None
 
     def next(self) -> int:
-        result = self.next_value
-        self.next_value = next(self.iterator, None)
+        result, self.next_value = self.next_value, next(self.inorder_generator, None)
         return result
 
-    def hasNext(self) -> bool:
+    def has_next(self) -> bool:
+        return self.next_value is not None
+
+
+class BSTIteratorStack:
+    """
+    Notes
+    ----
+    - TC: O(1) (average)
+    - SC: O(H)
+    """
+
+    def __init__(self, root: Optional[TreeNode]) -> None:
+        self.inorder_generator = self._generate_inorder_value(root)
+        self.next_value = next(self.inorder_generator, None)
+
+    def _generate_inorder_value(
+        self, node: Optional[TreeNode]
+    ) -> Generator[int, None, None]:
+        stack = []
+        while node or stack:
+            while node:
+                stack.append(node)
+                node = node.left
+
+            node = stack.pop()
+            yield node.val
+            node = node.right
+
+        self.next_value = None
+
+    def next(self) -> Optional[int]:
+        result, self.next_value = self.next_value, next(self.inorder_generator, None)
+        return result
+
+    def has_next(self) -> bool:
         return self.next_value is not None
