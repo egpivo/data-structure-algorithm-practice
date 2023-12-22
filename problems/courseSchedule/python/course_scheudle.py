210072d47f7e47c84e25668d14a759cc655328fa
egpivo <egpivo@gmail.com>
2023-12-22 11:06:29 +0800

    CHG: refine dfs solution; add bfs solution

--- problems/courseSchedule/python/course_scheudle.py
+++ problems/courseSchedule/python/course_scheudle.py
@@ -1,35 +1,76 @@
-from collections import defaultdict
+from collections import defaultdict, deque
 from typing import List
 
 
-class Solution:
+class SolutionDFS:
+    """
+    Notes
+    -----
+    - TC: O(V + E)
+    - SC: O(V + E)
+    """
+
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        if numCourses == 0:
+            return True
+
         graph = defaultdict(list)
 
-        for src, dst in prerequisites:
-            graph[src].append(dst)
+        for course, precourse in prerequisites:
+            graph[precourse].append(course)
 
-        def dfs(src, visited, path, memo):
-            if (src, path) in memo:
-                return memo[(src, path)]
-
-            if src in visited:
-                return False
-            if src not in graph:
+        def is_valid(course, visited, path, memory):
+            if (course, path) in memory:
+                return memory[(course, path)]
+            if not course in graph:
                 return True
 
-            visited.add(src)
+            visited.add(course)
             result = True
+            for neighbor in graph[course]:
+                if neighbor in visited or not is_valid(
+                    neighbor, visited, path + 1, memory
+                ):
+                    result = False
+                    break
+            visited.remove(course)
+            memory[(course, path)] = result
+            return memory[(course, path)]
 
-            for neighbor in graph[src]:
-                result &= dfs(neighbor, visited, path + 1, memo)
-
-            visited.remove(src)
-            memo[(src, path)] = result
-            return result
-
-        for key in range(numCourses):
-            is_valid = dfs(key, set(), 0, {})
-            if not is_valid:
+        for course in graph:
+            if not is_valid(course, set(), 0, dict()):
                 return False
         return True
+
+
+class SolutionBFS:
+    """
+    Notes
+    -----
+    - TC: O(V + E)
+    - SC: O(V + E)
+    """
+
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        if numCourses == 0:
+            return True
+        graph = defaultdict(list)
+        in_degree = [0] * numCourses
+
+        for course, prereq in prerequisites:
+            graph[prereq].append(course)
+            in_degree[course] += 1
+
+        queue = deque(course for course in range(numCourses) if in_degree[course] == 0)
+        visited = set()
+
+        while queue:
+            current_course = queue.popleft()
+            visited.add(current_course)
+
+            for neighbor in graph[current_course]:
+                in_degree[neighbor] -= 1
+                if in_degree[neighbor] == 0:
+                    queue.append(neighbor)
+
+        return len(visited) == numCourses
