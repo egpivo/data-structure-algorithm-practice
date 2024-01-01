1339a23248eb3a2aefe0d6af8df638cbd5e9b002
egpivo <egpivo@gmail.com>
2024-01-01 15:48:57 +0800

    CHG: refine solutions with complexity analysis

--- problems/mergeKSortedLinkedList/python/Merge_k_linked_list.py
+++ problems/mergeKSortedLinkedList/python/Merge_k_linked_list.py
@@ -1,30 +1,79 @@
+from heapq import heappop, heappush
 from typing import List, Optional
 
 
 # Definition for singly-linked list.
 class ListNode:
-    def __init__(self, x):
+    def __init__(self, x=0, next=None):
         self.val = x
         self.next = None
 
 
 class Solution:
+    """
+    Complexity
+    ----------
+    - Time complexity: $O(n\cdot k)$
+    - Space complexity: $O(1)$
+    """
+
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
         if not lists:
             return None
 
-        val_list = []
-        for node in lists:
-            while node:
-                val_list.append(node.val)
-                node = node.next
-        head = ListNode(None)
-        forward = head
+        result = lists[0]
+        for node in lists[1:]:
+            result = self.merge(result, node)
 
-        for val in val_list:
-            forward.next = ListNode(val)
-            forward = forward.next
-        return head.next
+        return result
+
+    def merge(
+        self, first: Optional[ListNode], second: Optional[ListNode]
+    ) -> Optional[ListNode]:
+        dummy = ListNode()
+        current = dummy
+
+        while first and second:
+            if first.val < second.val:
+                current.next, first = first, first.next
+            else:
+                current.next, second = second, second.next
+            current = current.next
+
+        current.next = first if first else second
+        return dummy.next
+
+
+class Solution2:
+    """
+    Complexity
+    ----------
+    - Time complexity: $O(n\log k)$
+    - Space complexity: $O(k)$
+    """
+
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        if not lists:
+            return None
+        k = len(lists)
+        result = ListNode(None)
+        current = result
+
+        priority_queue = []
+        for index in range(k):
+            if lists[index]:
+                heappush(priority_queue, (lists[index].val, index))
+                lists[index] = lists[index].next
+
+        while priority_queue:
+            value, index = heappop(priority_queue)
+            current.next = ListNode(value)
+            current = current.next
+            if lists[index]:
+                heappush(priority_queue, (lists[index].val, index))
+                lists[index] = lists[index].next
+
+        return result.next
 
 
 if __name__ == "__main__":
