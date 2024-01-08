5afe243863a3d97a9f06cddfb6cb8cd1f6527107
egpivo <egpivo@gmail.com>
2024-01-08 10:07:57 +0800

    CHG: add another solution

--- problems/numberOfOneBits/python/number_1_bits.py
+++ problems/numberOfOneBits/python/number_1_bits.py
@@ -11,6 +11,18 @@ class Solution:
         return sum((n & (1 << i)) != 0 for i in range(32))
 
 
+class Solution2:
+    def hammingWeight(self, n: int) -> int:
+        m = 31
+        count = 0
+
+        while n > 0:
+            count += n & 1
+            n >>= 1
+            m -= 1
+        return count
+
+
 if __name__ == "__main__":
     n = 11
     print(f"Solution: {Solution().hammingWeight(n)}")
