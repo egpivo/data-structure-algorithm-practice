7e5d96df9885f093fe0c1f3c9d4edb3e42fe203c
egpivo <egpivo@gmail.com>
2024-01-16 13:42:50 +0800

    CHG: add a refined solution

--- problems/insertDeleteGetRandom/python/insert_delete_get_random.py
+++ problems/insertDeleteGetRandom/python/insert_delete_get_random.py
@@ -40,6 +40,53 @@ class RandomizedSet:
         return choice(self.list)
 
 
+class RandomizedSetRefined:
+    def __init__(self):
+        # List to store the elements
+        self.elements = []
+        # Dictionary to map element to its index in the list
+        self.element_index_map = {}
+
+    def insert(self, val: int) -> bool:
+        # Check if the value already exists
+        if val in self.element_index_map:
+            return False
+        else:
+            # Add the value to the list and update the index in the dictionary
+            self.elements.append(val)
+            self.element_index_map[val] = len(self.elements) - 1
+            return True
+
+    def remove(self, val: int) -> bool:
+        # Check if the value exists
+        if val in self.element_index_map:
+            # Get the index of the element to remove
+            index_to_remove = self.element_index_map[val]
+
+            # Swap the element with the last one in the list
+            last_element = self.elements.pop()
+            if index_to_remove != len(self.elements):
+                self.elements[index_to_remove] = last_element
+                self.element_index_map[last_element] = index_to_remove
+
+            # Remove the element from the dictionary
+            del self.element_index_map[val]
+            return True
+        else:
+            return False
+
+    def getRandom(self) -> int:
+        # Use random.choice to directly select a random element from the list
+        return choice(self.elements)
+
+
+# Your RandomizedSet object will be instantiated and called as such:
+# obj = RandomizedSet()
+# param_1 = obj.insert(val)
+# param_2 = obj.remove(val)
+# param_3 = obj.getRandom()
+
+
 # Your RandomizedSet object will be instantiated and called as such:
 # obj = RandomizedSet()
 # param_1 = obj.insert(val)
--- problems/pow(x,n)/python/power.py
+++ problems/pow(x,n)/python/power.py
@@ -7,7 +7,7 @@ class Solution:
     """
 
     def power(self, x: float, n: int) -> float:
-        answer = self._recurse(x, n)
+        answer = self._recurse(x, abs(n))
         return answer if n >= 0 else 1 / answer
 
     def _recurse(self, base: int, exponent: int) -> float:
@@ -15,9 +15,9 @@ class Solution:
             return 1
 
         if exponent % 2 == 0:
-            return self._recurse(base * base, (abs(exponent)) // 2)
+            return self._recurse(base * base, exponent // 2)
         else:
-            return base * self._recurse(base * base, (abs(exponent) - 1) // 2)
+            return base * self._recurse(base * base, (exponent - 1) // 2)
 
 
 class SolutionIterative:
