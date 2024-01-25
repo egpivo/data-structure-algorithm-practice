cfeba99519b5b2462443324f3f8dc24fa0ddd0bb
egpivo <egpivo@gmail.com>
2024-01-25 11:12:11 +0800

    CHG: add another solution to the continuous subarray sum

--- problems/continuousSubarraySum/python/continuous_subarray_sum.py
+++ problems/continuousSubarraySum/python/continuous_subarray_sum.py
@@ -27,8 +27,32 @@ class Solution:
         return False
 
 
+class Solution2:
+    """
+
+    Complexity
+    ----------
+    - TC: O(n)
+    - SC: O(n)
+    """
+
+    def checkSubarraySum(self, nums: List[int], k: int) -> bool:
+        nums[0] %= k
+        seen_remainders = {0}
+
+        for i in range(1, len(nums)):
+            nums[i] = (nums[i] + nums[i - 1]) % k
+            if nums[i] in seen_remainders:
+                return True
+            seen_remainders.add(nums[i - 1])
+        return False
+
+
 if __name__ == "__main__":
     nums = [23, 2, 6, 4, 7]
 
     print(f"Solution: {Solution().checkSubarraySum(nums, 6)}")
     print(f"Solution: {Solution().checkSubarraySum(nums, 9)}")
+
+    print(f"Solution: {Solution2().checkSubarraySum(nums, 6)}")
+    print(f"Solution: {Solution2().checkSubarraySum(nums, 9)}")
