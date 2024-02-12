45e8d9c9f15421a2bfe4a386de0c3d60184e5b74
egpivo <egpivo@gmail.com>
2024-02-12 10:05:07 +0800

    CHG: add a new solution to the problem of finding the majority element

--- problems/majorityElement/python/majority_element.py
+++ problems/majorityElement/python/majority_element.py
@@ -1,3 +1,6 @@
+from collections import Counter
+
+
 class Solution:
     def majorityElement(self, nums):
         """
@@ -32,9 +35,24 @@ class Solution2:
         return mode
 
 
+class Solution3:
+    """
+    Complexity
+    ----------
+    - TC: O(n)
+    - SC: O(n)
+    """
+
+    def majorityElement(self, nums):
+        freq = Counter(nums)
+        return freq.most_common(1)[0][0]
+
+
 if __name__ == "__main__":
     nums = [2, 2, 1, 1, 1, 2, 2]
     ans = Solution()
     print(ans.majorityElement(nums))
     ans2 = Solution2()
     print(ans2.majorityElement(nums))
+    ans3 = Solution3()
+    print(ans3.majorityElement(nums))
