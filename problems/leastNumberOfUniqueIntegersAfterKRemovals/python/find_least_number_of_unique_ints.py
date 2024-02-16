c6811ae016fcb9fe353a57bbabd1b760f4f4fd1e
egpivo <egpivo@gmail.com>
2024-02-16 09:55:24 +0800

    CHG: add an efficient solution

--- problems/leastNumberOfUniqueIntegersAfterKRemovals/python/find_least_number_of_unique_ints.py
+++ problems/leastNumberOfUniqueIntegersAfterKRemovals/python/find_least_number_of_unique_ints.py
@@ -2,7 +2,14 @@ from collections import Counter
 from typing import List
 
 
-class Solution:
+class Solution1:
+    """
+    Complexity
+    ----------
+    - TC: O(n\log(n))
+    - SC: O(n)
+    """
+
     def findLeastNumOfUniqueInts(self, arr: List[int], k: int) -> int:
         freq = Counter(arr)
         unique_count = len(freq)
@@ -17,7 +24,31 @@ class Solution:
         return unique_count
 
 
+class Solution2:
+    """
+    Complexity
+    ----------
+    - TC: O(n)
+    - SC: O(n)
+    """
+
+    def findLeastNumOfUniqueInts(self, arr: List[int], k: int) -> int:
+        freqs = Counter(arr)
+        unique_count = len(freqs)
+        historgram = Counter(freqs.values())
+
+        for freq in range(1, len(arr) + 1):
+            if k >= freq * historgram[freq]:
+                k -= freq * historgram[freq]
+                unique_count -= historgram[freq]
+            else:
+                break
+
+        return unique_count - k // freq
+
+
 if __name__ == "__main__":
-    arr = [4, 3, 1, 1, 3, 3, 2]
+    arr = [2, 4, 1, 8, 3, 5, 1, 3]
     k = 3
-    print(f"Solution: {Solution().findLeastNumOfUniqueInts(arr, k)}")
+    print(f"Solution: {Solution1().findLeastNumOfUniqueInts(arr, k)}")
+    print(f"Solution: {Solution2().findLeastNumOfUniqueInts(arr, k)}")
