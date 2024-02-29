621e3a5d99292dfdd2015c7c0bcff4e2966415d9
egpivo <egpivo@gmail.com>
2024-02-29 09:48:21 +0800

    CHG: add a new problem for checking even-odd tree

---
+++ problems/evenOddTree/python/is_even_odd_tree.py
@@ -0,0 +1,52 @@
+from collections import deque
+from sys import maxsize
+from typing import Optional
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def isEvenOddTree(self, root: Optional[TreeNode]) -> bool:
+        if not root:
+            return True  # An empty tree is considered an even-odd tree
+
+        queue = deque([root])
+        level = 0
+
+        while queue:
+            size = len(queue)
+            prev_val = -maxsize if level % 2 == 0 else maxsize
+
+            for _ in range(size):
+                node = queue.popleft()
+
+                if (level % 2 == 0 and (prev_val >= node.val or node.val % 2 == 0)) or (
+                    level % 2 == 1 and (prev_val <= node.val or node.val % 2 == 1)
+                ):
+                    return False
+
+                prev_val = node.val
+
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+
+            level += 1
+
+        return True
+
+
+if __name__ == "__main__":
+    tree = TreeNode(1)
+    tree.right = TreeNode(2)
+    tree.right.left = TreeNode(3)
+    tree.right.right = TreeNode(5)
+
+    print(f"The BFS answer is {Solution().isEvenOddTree(tree)}")
