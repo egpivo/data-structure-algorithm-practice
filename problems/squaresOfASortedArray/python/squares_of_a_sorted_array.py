d089a4e26bb59f021c178ea9eb92bbd33a21981b
egpivo <egpivo@gmail.com>
2024-03-02 12:26:36 +0800

    CHG: add a new solution with same complexity order

--- problems/squaresOfASortedArray/python/squares_of_a_sorted_array.py
+++ problems/squaresOfASortedArray/python/squares_of_a_sorted_array.py
@@ -6,7 +6,7 @@ class Solution:
     Complexity
     ----------
     - TC: O(N)
-    - SC: O(1)
+    - SC: O(N)
     """
 
     def sortedSquares(self, nums: List[int]) -> List[int]:
@@ -23,6 +23,43 @@ class Solution:
         return answer[::-1]
 
 
+class Solution2:
+    """
+    Complexity
+    ----------
+    - TC: O(N)
+    - SC: O(N)
+    """
+
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+
+        # Find the index where positive numbers start
+        right = next((i for i, num in enumerate(nums) if num > 0), n)
+
+        left = right - 1
+        result = []
+
+        # Merge the squared values of the left and right pointers
+        while left >= 0 and right < n:
+            left_squared, right_squared = nums[left] ** 2, nums[right] ** 2
+            if left_squared < right_squared:
+                result.append(left_squared)
+                left -= 1
+            else:
+                result.append(right_squared)
+                right += 1
+
+        # Add remaining squared values from the left pointer
+        result.extend(nums[left] ** 2 for left in range(left, -1, -1))
+
+        # Add remaining squared values from the right pointer
+        result.extend(nums[right] ** 2 for right in range(right, n))
+
+        return result
+
+
 if __name__ == "__main__":
     nums = [-4, -1, 0, 3, 10]
     print(f"{Solution().sortedSquares(nums)}")
+    print(f"{Solution2().sortedSquares(nums)}")
