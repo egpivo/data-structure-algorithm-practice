5e33d041c5bbee76aa568b14c94b00602e10c987
egpivo <egpivo@gmail.com>
2024-03-05 09:19:25 +0800

    CHG: add a new problem for minimum length of string

---
+++ problems/minimumLengthOfStringAfterDeletingSimilarEnds/python/minimum_length_of_srtring.py
@@ -0,0 +1,21 @@
+class Solution:
+    def minimumLength(self, s: str) -> int:
+        left, right = 0, len(s) - 1
+
+        while left < right and s[left] == s[right]:
+            current_char = s[left]
+
+            # Skip consecutive characters from the left
+            while left <= right and s[left] == current_char:
+                left += 1
+
+            # Skip consecutive characters from the right
+            while left <= right and s[right] == current_char:
+                right -= 1
+
+        return max(0, right - left + 1)
+
+
+if __name__ == "__main__":
+    s = "aabccabba"
+    print(f"{Solution().minimumLength(s)}")
